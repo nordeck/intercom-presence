95ad06ee9d057077bdf6b31f87814deda2543806
emrah <emrah.eryilmaz@nordeck.net>
2024-12-05 12:34:59 +0300

    initial

---
+++ README.md
@@ -0,0 +1,82 @@
+# Presence service with NATS
+
+## NATS server
+
+Open https://github.com/nats-io/nats-server/releases/latest and download the
+matching package. e.g. `nats-server-v2.10.22-linux-amd64.zip` into `/tmp`.
+
+```bash
+cd /tmp
+unzip nats-server-v2.10.22-linux-amd64.zip
+cp /tmp/nats-server-v2.10.22-linux-amd64/nats-server ~/bin/
+```
+
+To run:
+
+```bash
+nats-server
+```
+
+## Deno
+
+As root
+
+```bash
+cd /tmp
+wget -O deno.zip https://github.com/denoland/deno/releases/latest/download/deno-x86_64-unknown-linux-gnu.zip
+unzip deno.zip
+./deno --version
+
+cp /tmp/deno /usr/local/bin/
+deno --version
+```
+
+## Presence service
+
+In this directory:
+
+```bash
+deno --allow-net presence.ts
+```
+
+## NATS CLI
+
+Open https://github.com/nats-io/natscli/releases/latest and download the
+matching package. e.g. `nats-0.1.5-linux-amd64.zip` into `/tmp`
+
+```bash
+cd /tmp
+unzip nats-0.1.5-linux-amd64.zip
+cp /tmp/nats-0.1.5-linux-amd64/nats ~/bin/
+```
+
+## Testing
+
+```bash
+USER1_KEYCLOAK_SUB="f:d704f61d-fade-4641-b03a-1f211206c5b6:user1"
+USER2_KEYCLOAK_SUB="f:d704f61d-fade-4641-b03a-1f211206c5b6:user2"
+USER1_KEYCLOAK_USERINFO='{"sub":"f:d704f61d-fade-4641-b03a-1f211206c5b6:user1","email_verified":true,"preferred_username":"user1","email":"user1@example.com"}'
+USER2_KEYCLOAK_USERINFO='{"sub":"f:d704f61d-fade-4641-b03a-1f211206c5b6:user2","email_verified":false,"preferred_username":"user2","email":"user2@example.net"}'
+
+nats -s "127.0.0.1" pub presence.update "$USER1_KEYCLOAK_USERINFO"
+nats -s "127.0.0.1" pub presence.update "$USER2_KEYCLOAK_USERINFO"
+
+nats -s "127.0.0.1" req presence.all ""
+nats -s "127.0.0.1" req presence.online ""
+nats -s "127.0.0.1" req presence.online "30"
+
+nats -s "127.0.0.1" req presence.whoami "$USER1_KEYCLOAK_SUB"
+nats -s "127.0.0.1" req presence.whoami "$USER2_KEYCLOAK_SUB"
+nats -s "127.0.0.1" req presence.isonline "$USER1_KEYCLOAK_SUB"
+nats -s "127.0.0.1" req presence.isonline "$USER2_KEYCLOAK_SUB"
+
+nats -s "127.0.0.1" pub presence.ping "$USER1_KEYCLOAK_SUB"
+nats -s "127.0.0.1" pub presence.ping "$USER2_KEYCLOAK_SUB"
+```
+
+## Links
+
+- https://github.com/nats-io/nats-server/releases/latest
+- https://github.com/nats-io/natscli/releases/latest
+- https://github.com/nats-io/nats.js/
+- https://examples.nats.io/examples/messaging/pub-sub/deno
---
+++ presence.ts
@@ -0,0 +1,173 @@
+import { v5 as uuid } from "jsr:@std/uuid@^1.0.0";
+import { connect } from "jsr:@nats-io/transport-deno@3.0.0-18";
+import type { NatsConnection } from "jsr:@nats-io/nats-core@3.0.0-46";
+
+const SERVERS = { servers: "127.0.0.1:4222" };
+const ONLINE_TIMEOUT_SECONDS = 120;
+const UUID_NAMESPACE = "6ba7b810-9dad-11d1-80b4-00c04fd430c8";
+
+interface UserInfo {
+  sub: string;
+  presenceEpoch: number;
+  presenceDate: string;
+  [key: string]: unknown;
+}
+
+const usersAll = new Map<string, UserInfo>();
+
+// -----------------------------------------------------------------------------
+// subPresenceUpdate: Add or update (if already exists) the user's info.
+// -----------------------------------------------------------------------------
+async function subPresenceUpdate(nc: NatsConnection) {
+  const sub = nc.subscribe("presence.update");
+
+  for await (const m of sub) {
+    try {
+      const userInfo = JSON.parse(m.string()) as UserInfo;
+      if (!userInfo?.sub) throw "not valid userInfo";
+
+      const presenceSub = new TextEncoder().encode(userInfo.sub);
+      const presenceId = await uuid.generate(UUID_NAMESPACE, presenceSub);
+
+      const time = new Date();
+      userInfo.presenceEpoch = time.getTime();
+      userInfo.presenceTime = time.toISOString();
+
+      usersAll.set(presenceId, userInfo);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// subPresencePing: Update the last seen value of the user.
+// -----------------------------------------------------------------------------
+async function subPresencePing(nc: NatsConnection) {
+  const sub = nc.subscribe("presence.ping");
+
+  for await (const m of sub) {
+    try {
+      if (!m.string()) throw "not valid key";
+
+      const presenceSub = new TextEncoder().encode(m.string());
+      const presenceId = await uuid.generate(UUID_NAMESPACE, presenceSub);
+      const userInfo = usersAll.get(presenceId);
+
+      if (userInfo) {
+        const time = new Date();
+        userInfo.presenceEpoch = time.getTime();
+        userInfo.presenceTime = time.toISOString();
+
+        usersAll.set(presenceId, userInfo);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// replyPresenceAll: Return the list of all users.
+// -----------------------------------------------------------------------------
+async function replyPresenceAll(nc: NatsConnection) {
+  const sub = nc.subscribe("presence.all");
+
+  for await (const m of sub) {
+    try {
+      m.respond(JSON.stringify([...usersAll]));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// replyPresenceOnline: Return the list of online users.
+// -----------------------------------------------------------------------------
+async function replyPresenceOnline(nc: NatsConnection) {
+  const sub = nc.subscribe("presence.online");
+
+  for await (const m of sub) {
+    try {
+      const usersOnline = [] as UserInfo[];
+
+      let lastSeenSecond = Date.now() - ONLINE_TIMEOUT_SECONDS * 1000;
+      const input = m.string();
+      if (input && input.match(/^\d+$/)) {
+        lastSeenSecond = Date.now() - Number(input) * 1000;
+      }
+
+      usersAll.forEach((user, _k) => {
+        if (user.presenceEpoch > lastSeenSecond) {
+          usersOnline.push(user);
+        }
+      });
+
+      m.respond(JSON.stringify(usersOnline));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// replyPresenceWhoami: Return the user info of a specific user.
+// -----------------------------------------------------------------------------
+async function replyPresenceWhoami(nc: NatsConnection) {
+  const sub = nc.subscribe("presence.whoami");
+
+  for await (const m of sub) {
+    try {
+      if (!m.string()) throw "not valid key";
+
+      const presenceSub = new TextEncoder().encode(m.string());
+      const presenceId = await uuid.generate(UUID_NAMESPACE, presenceSub);
+
+      m.respond(JSON.stringify(usersAll.get(presenceId)));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// replyPresenceIsOnline: Return the online status of a specific user.
+// -----------------------------------------------------------------------------
+async function replyPresenceIsOnline(nc: NatsConnection) {
+  const sub = nc.subscribe("presence.isonline");
+
+  for await (const m of sub) {
+    try {
+      if (!m.string()) throw "not valid key";
+
+      const presenceSub = new TextEncoder().encode(m.string());
+      const presenceId = await uuid.generate(UUID_NAMESPACE, presenceSub);
+      const lastSeenSecond = Date.now() - ONLINE_TIMEOUT_SECONDS * 1000;
+      const userInfo = usersAll.get(presenceId);
+
+      if (userInfo && userInfo.presenceEpoch > lastSeenSecond) {
+        m.respond(JSON.stringify(true));
+        continue;
+      }
+
+      m.respond(JSON.stringify(false));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// main
+// -----------------------------------------------------------------------------
+const nc = await connect(SERVERS);
+
+subPresenceUpdate(nc);
+subPresencePing(nc);
+replyPresenceAll(nc);
+replyPresenceOnline(nc);
+replyPresenceWhoami(nc);
+replyPresenceIsOnline(nc);
+
+await nc.closed();
