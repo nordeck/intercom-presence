12c2aff0bcdba8b0af422e6d4f4bb622ccfbd48e
emrah <emrah.eryilmaz@nordeck.net>
2024-12-18 14:39:14 +0300

    feat: add nats into notification.ts

--- notification.ts
+++ notification.ts
@@ -1,6 +1,12 @@
-const HOSTNAME = "0.0.0.0";
-const PORT = 8000;
+import { v5 as uuid } from "jsr:@std/uuid@^1.0.0";
+import { connect } from "jsr:@nats-io/transport-deno@3.0.0-18";
+import type { NatsConnection } from "jsr:@nats-io/nats-core@3.0.0-46";
+
+const NATS_SERVERS = { servers: "127.0.0.1:4222" };
+const HTTP_HOSTNAME = "0.0.0.0";
+const HTTP_PORT = 8000;
 const PRE = "/intercom";
+const UUID_NAMESPACE = "6ba7b810-9dad-11d1-80b4-00c04fd430c8";
 
 // -----------------------------------------------------------------------------
 export function methodNotAllowed(): Response {
@@ -12,6 +18,9 @@ export function methodNotAllowed(): Response {
 
   return new Response(JSON.stringify(body), {
     status: 405,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 }
 
@@ -25,21 +34,73 @@ export function notFound(): Response {
 
   return new Response(JSON.stringify(body), {
     status: 404,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   });
 }
 
 // -----------------------------------------------------------------------------
-function createStream(): ReadableStream {
-  const stream = new ReadableStream({
-    start(controller) {
-      setInterval(() => {
-        const encoder = new TextEncoder();
-        const data = encoder.encode("data: hello\n\n");
-        controller.enqueue(data);
-      }, 3000);
+export function unauthorized(): Response {
+  const body = {
+    error: {
+      message: "Unauthorized",
     },
-    cancel(reason) {
-      console.log(reason);
+  };
+
+  return new Response(JSON.stringify(body), {
+    status: 401,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
+
+// -----------------------------------------------------------------------------
+// It returns the hardcoded pub channel for now. Auth checking will be added.
+// -----------------------------------------------------------------------------
+async function getChannel(req: Request): Promise<string> {
+  const url = new URL(req.url);
+  const qs = new URLSearchParams(url.search);
+  const user = qs.get("user");
+  if (!user) throw "no user";
+
+  const encoder = new TextEncoder();
+
+  if (user === "user1") {
+    const id = encoder.encode("f:d704f61d-fade-4641-b03a-1f211206c5b6:user1");
+    const userUuid = await uuid.generate(UUID_NAMESPACE, id);
+    return `notification.${userUuid}`;
+  } else if (user === "user2") {
+    const id = encoder.encode("f:d704f61d-fade-4641-b03a-1f211206c5b6:user2");
+    const userUuid = await uuid.generate(UUID_NAMESPACE, id);
+    return `notification.${userUuid}`;
+  } else {
+    throw "invalid user";
+  }
+}
+
+// -----------------------------------------------------------------------------
+function createStream(channel: string): ReadableStream {
+  console.log(channel);
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder();
+      const sub = nc.subscribe(channel);
+
+      for await (const m of sub) {
+        try {
+          console.log(m);
+          const data = encoder.encode(`data: coming message\n\n`);
+          controller.enqueue(data);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    },
+    cancel() {
+      console.log("cancelled");
     },
   });
 
@@ -47,19 +108,23 @@ function createStream(): ReadableStream {
 }
 
 // -----------------------------------------------------------------------------
-function notification(): Response {
-  const stream = createStream();
+async function notification(req: Request): Promise<Response> {
+  const channel = await getChannel(req);
+  if (!channel) return unauthorized();
+
+  const stream = createStream(channel);
   const headers = {
     "Content-Type": "text/event-stream",
     "Cache-Control": "no-cache",
     "Connection": "keep-alive",
+    "Access-Control-Allow-Origin": "*",
   };
 
   return new Response(stream, { headers });
 }
 
 // -----------------------------------------------------------------------------
-function handler(req: Request): Response {
+async function handler(req: Request): Promise<Response> {
   // check method
   if (req.method !== "GET") {
     return methodNotAllowed();
@@ -69,19 +134,23 @@ function handler(req: Request): Response {
   const path = url.pathname;
 
   if (path === `${PRE}/notification`) {
-    return notification();
+    return await notification(req);
   } else {
     return notFound();
   }
 }
 
 // -----------------------------------------------------------------------------
-function main() {
+function sseServer() {
   Deno.serve({
-    hostname: HOSTNAME,
-    port: PORT,
+    hostname: HTTP_HOSTNAME,
+    port: HTTP_PORT,
   }, handler);
 }
 
 // -----------------------------------------------------------------------------
-main();
+const nc = await connect(NATS_SERVERS) as NatsConnection;
+
+sseServer();
+
+await nc.closed();
