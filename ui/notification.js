8f6a4b089c491afd48e569f53a5a18efdc89ff46
emrah <emrah.eryilmaz@nordeck.net>
2024-12-19 14:56:08 +0300

    feat(notification): add toaster

--- ui/notification.js
+++ ui/notification.js
@@ -1,9 +1,51 @@
+let i = 0;
+
+// -----------------------------------------------------------------------------
+// removeNotification
+// -----------------------------------------------------------------------------
+function removeNotification(msgId, msgDiv) {
+  if (!msgId) throw "invalid message id";
+  if (!msgDiv) throw "missing message div";
+  if (!globalThis.notification[`call-${msgId}`]) throw "missing notification";
+
+  if (Number(globalThis.notification[`call-${msgId}`]) > Date.now() - 3000) {
+    setTimeout(() => {
+      removeNotification(msgId, msgDiv);
+    }, 1000);
+
+    return;
+  }
+
+  console.error("remove");
+  msgDiv.remove();
+}
+
 // -----------------------------------------------------------------------------
 // callHandler
 // -----------------------------------------------------------------------------
 function callHandler(data) {
   console.log(data);
   console.log(data?.callee);
+
+  const msgId = data?.id;
+  if (!msgId) throw "invalid id";
+
+  if (globalThis.notification[`call-${msgId}`]) {
+    globalThis.notification[`call-${msgId}`] = Date.now();
+    return;
+  }
+
+  const toast = document.getElementById("notificationContainer");
+  const msgDiv = document.createElement("div");
+  msgDiv.textContent = `${data?.callee} - ${i}`;
+  toast.appendChild(msgDiv);
+  i = i + 1;
+
+  globalThis.notification[`call-${msgId}`] = Date.now();
+
+  setTimeout(() => {
+    removeNotification(msgId, msgDiv);
+  }, 1000);
 }
 
 // -----------------------------------------------------------------------------
@@ -23,7 +65,10 @@ function onMessage(e) {
 // -----------------------------------------------------------------------------
 // main
 // -----------------------------------------------------------------------------
-const user = globalThis.user;
+// Create a namespace if it doesn't exist.
+globalThis.notification = globalThis.notification || {};
+
+const user = globalThis.notification.user;
 const src = `http://127.0.0.1:8000/intercom/notification?user=${user}`;
 
 const el = document.getElementById("user");
