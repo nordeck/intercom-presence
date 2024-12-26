6c46f6b2319de9c7e87e9ca6b8b1bf69d2973c0a
emrah <emrah.eryilmaz@nordeck.net>
2024-12-26 10:12:42 +0300

    feat(notification.js): reject and accept buttons

--- ui/notification.js
+++ ui/notification.js
@@ -90,9 +90,11 @@ function createIcon(pathData, width, height, color) {
   icon.setAttribute("fill", color);
   icon.setAttribute("viewBox", "0 0 16 16");
 
-  const path = document.createElementNS(ns, "path");
-  path.setAttribute("d", pathData);
-  icon.appendChild(path);
+  for (const p of pathData) {
+    const path = document.createElementNS(ns, "path");
+    path.setAttribute("d", p);
+    icon.appendChild(path);
+  }
 
   return icon;
 }
@@ -101,10 +103,11 @@ function createIcon(pathData, width, height, color) {
 // closeIcon
 // -----------------------------------------------------------------------------
 function closeIcon() {
-  const pathData =
+  const pathData = [
     "M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 " +
     ".708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 " +
-    "2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708";
+    "2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708",
+  ];
 
   return createIcon(pathData, 16, 16, "currentColor");
 }
@@ -113,7 +116,7 @@ function closeIcon() {
 // phoneIcon
 // -----------------------------------------------------------------------------
 function phoneIcon() {
-  const pathData =
+  const pathData = [
     "M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 " +
     "1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 " +
     "4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 " +
@@ -124,9 +127,44 @@ function phoneIcon() {
     "2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 " +
     "1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 " +
     "1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 " +
-    "1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z";
+    "1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z",
+  ];
 
-  return createIcon(pathData, 24, 24, "blue");
+  return createIcon(pathData, 16, 16, "blue");
+}
+
+// -----------------------------------------------------------------------------
+// rejectIcon
+// -----------------------------------------------------------------------------
+function rejectIcon() {
+  const pathData = [
+    "M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 " +
+    ".708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 " +
+    "5.147a.5.5 0 0 1-.708-.708L7.293 8z",
+  ];
+
+  return createIcon(pathData, 18, 18, "white");
+}
+
+// -----------------------------------------------------------------------------
+// acceptIcon
+// -----------------------------------------------------------------------------
+function acceptIcon() {
+  const pathData = [
+    "M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 " +
+    "1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 " +
+    "4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 " +
+    "0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 " +
+    "1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 " +
+    "0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 " +
+    "2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 " +
+    "2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 " +
+    "1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 " +
+    "1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 " +
+    "1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z",
+  ];
+
+  return createIcon(pathData, 18, 18, "white");
 }
 
 // -----------------------------------------------------------------------------
@@ -170,6 +208,7 @@ function createCallPopup(callId, callerName) {
   close.style.margin = "0px 0px auto auto";
   close.style.border = "none";
   close.style.backgroundColor = "#fff";
+  close.style.cursor = "pointer";
   close.appendChild(closeIcon());
   header.appendChild(close);
 
@@ -184,12 +223,24 @@ function createCallPopup(callId, callerName) {
 
   // Body, reject button
   const reject = document.createElement("button");
-  reject.appendChild(closeIcon());
+  reject.style.margin = "4px 24px";
+  reject.style.padding = "8px";
+  reject.style.border = "none";
+  reject.style.borderRadius = "25px";
+  reject.style.backgroundColor = "#FF4D4D";
+  reject.style.cursor = "pointer";
+  reject.appendChild(rejectIcon());
   body.appendChild(reject);
 
   // Body, accept button
   const accept = document.createElement("button");
-  accept.appendChild(closeIcon());
+  accept.style.margin = "4px 24px";
+  accept.style.padding = "8px";
+  accept.style.border = "none";
+  accept.style.borderRadius = "25px";
+  accept.style.backgroundColor = "#4CAF50";
+  accept.style.cursor = "pointer";
+  accept.appendChild(acceptIcon());
   body.appendChild(accept);
 
   return callPopup;
